466e0568f679ba079d2dc603be5a8ccd56faa608
harikuttan <harikuttan@hareendranath.(none)>
2014-09-28 12:37:41 +0530

    rest api added for questionaire

--- config/autoload/doctrineorm.local.php
+++ config/autoload/doctrineorm.local.php
@@ -22,7 +22,7 @@ return array(
                     'host'     => 'localhost',
                     'port'     => '3306',
                     'user'     => 'root',
-                    'password' => 'datshari009',
+                    'password' => 'root',
                     'dbname'   => 'cap',
 					'charset' => 'utf8', // extra
 					'driverOptions' => array(
